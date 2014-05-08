91d477d3bae6e207edda5c9608d698302460c006
male110 <male110@eyou.com>
2014-05-08 15:02:10 +0800

    　

---
+++ src/System/Function/Common.go
@@ -0,0 +1,12 @@
+package Function
+
+import (
+	"os"
+)
+
+// 检查文件或目录是否存在
+// 如果由 filename 指定的文件或目录存在则返回 true，否则返回 false
+func FileExist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil || os.IsExist(err)
+}
